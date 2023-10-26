f26dc62d959da87716dbf2299bb7ae83516efee9
Luckysharad <148042217+Luckysharad@users.noreply.github.com>
2023-10-26 12:40:02 +0530

    2nd commit

---
+++ Ascii.py
@@ -0,0 +1,11 @@
+print ("Hello, World!")
+char = "a"
+# ord() method is used for converting any character into ascii value
+print (ord(char))
+char = "A"
+print (ord(char))
+char = " " #ascii value of space
+print (ord(char))
+ascii = 65
+# chr() function is used for converting ascii value into character 
+print (chr(ascii))
